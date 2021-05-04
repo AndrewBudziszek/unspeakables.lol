92832a2d29bb38b0f043718355254ce41849565f
Andrew Budziszek <andrew@budziszek.com>
2021-05-04 16:19:21 -0500

    can't remember what I did

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Shuffler from './components/Shuffler'
 import FreeBreakfastIcon from '@material-ui/icons/FreeBreakfast';
 import React, { useState, useEffect } from 'react'
+import { Twitter } from '@material-ui/icons';
 
 
 function App() {
@@ -13,7 +14,17 @@ function App() {
     }}>
       <header className="App-header">
         <Shuffler backgroundColorCallback={setBackgroundColor} />
-        <Link href="https://www.buymeacoffee.com/SonBrooks" target="_blank" variant="body2"><FreeBreakfastIcon /></Link>
+        <Link href="https://www.buymeacoffee.com/SonBrooks" target="_blank" variant="caption"><FreeBreakfastIcon /></Link>
+        <Link href="https://twitter.com/unspeakableslol" target="_blank" variant="caption">
+          <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            flexWrap: 'wrap',
+          }}>
+            <Twitter />
+            <span>Submit requests</span>
+          </div>
+        </Link>
       </header>
     </div>
   );
